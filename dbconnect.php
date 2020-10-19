df1fc4f3902f0a1b47b8a7b0fbcf5803e867b968
secretangle204 <64527972+secretangle204@users.noreply.github.com>
2020-10-19 10:41:10 +0700

    Add files via upload

--- dbconnect.php
+++ dbconnect.php
@@ -1,9 +1,9 @@
 <?php
 
-$server = "localhost";
-$username = "postgres";
-$password = "secretangle204";
-$database = "test";
-$connString = "host=$server dbname=$database user=$username password=$password";
+	$server = "ec2-54-157-4-216.compute-1.amazonaws.com";
+	$username = "dhphcmlyaavjdu";
+	$password = "9e257fde734b6d700b44a6ae27427be8a9aae2969ad43d02e5637a8371b24c47";
+	$database = "dq4kijmpn30ff";
+	$connString = "host=$server dbname=$database user=$username password=$password";
 
 ?>
