6090e3bfe28328aeb9c4aa5643d720e6ba7da8b7
secretangle204 <64527972+secretangle204@users.noreply.github.com>
2020-10-20 20:22:47 +0700

    Add files via upload

--- dbconnect.php
+++ dbconnect.php
@@ -1,9 +1,8 @@
 <?php
-
-	$server = "ec2-54-157-4-216.compute-1.amazonaws.com";
-	$username = "dhphcmlyaavjdu";
-	$password = "9e257fde734b6d700b44a6ae27427be8a9aae2969ad43d02e5637a8371b24c47";
-	$database = "dq4kijmpn30ff";
+	$server = "ec2-18-211-86-133.compute-1.amazonaws.com";
+	$username = "anwopaxwjwboqs";
+	$password = "268222f280c912cffef1e932bb273cc102bebe3da0b913473a8abc348d32ff41";
+	$database = "d63eb1rufkr7u9";
 	$connString = "host=$server dbname=$database user=$username password=$password";
-
+	
 ?>
